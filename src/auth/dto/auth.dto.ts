06eeeb7c7c8a67dfccbe137761cbbbb5dbffe155
MedAmine2221 <lazregamine258@gmail.com>
2026-05-04 21:26:45 +0200

    ajouter ders exemple de json pour l'api auth

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,10 +1,13 @@
 /* eslint-disable prettier/prettier */
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 export class AuthPayloadDTO {
+  @ApiProperty({ example: 'zeinebmeriem.boukadida@polytechnicien.tn' })
   @IsEmail()
   email: string;
 
+  @ApiProperty({ example: 'Admin@123' })
   @IsString()
   password: string;
 }
